b0b638e8a1658c035db882a853b063c9e1b95daa
AdnanFauzanR <adnanfauzanr29@gmail.com>
2023-03-26 17:47:13 +0800

    init

--- src/components/CardKomoditiCategory.js
+++ src/components/CardKomoditiCategory.js
@@ -4,16 +4,16 @@ import button from './../images/Button2.svg';
 
 const CardKomoditiCategory = (props) => {
     return (
-        <div className="CardCategory">
-            <div className="CardCategory-content">
-                <div className="CardCategory-logo">
+        <div className="CardKomoditiCategory">
+            <div className="CardKomoditiCategory-content">
+                <div className="CardKomoditiCategory-logo">
                     <img src={props.komoditi.image}/>
                 </div>
-                <div className="CardCategory-title">
+                <div className="CardKomoditiCategory-title">
                     <p id="Sum-Category">{props.komoditi.count}</p>
                     <p id="Title">{props.komoditi.name}</p>
                 </div>
-                <a href="#" className="CardCategory-button">
+                <a href="#" className="CardKomoditiCategory-button">
                     <img src={button}/>
                 </a>
             </div>
